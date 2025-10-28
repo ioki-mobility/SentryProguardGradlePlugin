3e3e577e71a9eebc01c4b03cdf135cc98d691ea8
StefMa <StefMaDev@outlook.com>
2025-10-28 10:05:17 +0100

    Prepare next release

--- src/test/kotlin/com/ioki/sentry/proguard/gradle/plugin/SentryProguardGradlePluginPublishingTest.kt
+++ src/test/kotlin/com/ioki/sentry/proguard/gradle/plugin/SentryProguardGradlePluginPublishingTest.kt
@@ -35,7 +35,7 @@ class SentryProguardGradlePluginPublishingTest {
         val buildFile = testTmpPath.resolve("build.gradle.kts")
         val newBuildFile = buildFile.readText().replace(
             oldValue = """id("com.ioki.sentry.proguard")""",
-            newValue = """id("com.ioki.sentry.proguard") version "2.4.0-SNAPSHOT""""
+            newValue = """id("com.ioki.sentry.proguard") version "2.4.0""""
         )
         buildFile.writeText(newBuildFile)
         val settingsFile = testTmpPath.resolve("settings.gradle.kts")
